e061dd27d95c3b1f27ab258c87dca0f199212de7
Piotr Kowalczyk <piotr.kowalczyk@isobar.com>
2021-11-28 22:38:32 +0100

    'banner+header'

---
+++ airbnb/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Banner from '../components/Banner'
+import Header from '../components/Header'
+
+export default function Home() {
+    return (
+        <div className="">
+            <Head>
+                <title>AirBnB</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+
+            <Header />
+            <Banner />
+
+            <main>
+                <section>
+                    <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+                </section>
+            </main>
+        </div>
+    )
+}
